23190a50546c5dd4115cb85ac6667d20662d37b9
Shohidul Islam <hridoy.jurain@yahoo.com>
2019-09-18 02:58:44 +0600

    1st commit

---
+++ src/main/java/com/example/uportal/UportalApplication.java
@@ -0,0 +1,13 @@
+package com.example.uportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UportalApplication.class, args);
+	}
+
+}
